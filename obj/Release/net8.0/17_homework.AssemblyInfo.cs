84a7740ee90f7bba6aa817e4f9a1923063a343d3
Artem chelyk <aartemchelyk@gmail.com>
2025-03-06 17:25:20 +0200

    папаап

--- obj/Release/net8.0/17_homework.AssemblyInfo.cs
+++ obj/Release/net8.0/17_homework.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("17_homework")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b9fc258395250e3fcd6244cda2b36e0781c52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("17_homework")]
